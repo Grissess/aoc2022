3212bdb94a549070d738463a0024f4513a50a708
Grissess <grissess@nexusg.org>
2022-12-04 00:24:48 -0500

    Day 4

---
+++ day4.py
@@ -0,0 +1,22 @@
+import sys
+
+def intv(x):
+    l, _, h = x.partition('-')
+    return int(l), int(h)
+
+def olap(il, ih, ol, oh):
+    return il >= ol and ih <= oh
+
+running = 0
+for line in sys.stdin:
+    line = line.strip()
+    if not line:
+        continue
+    l, _, r = line.partition(',')
+    ll, lh = intv(l)
+    rl, rh = intv(r)
+    if olap(ll, lh, rl, rh) or olap(rl, rh, ll, lh):
+        running += 1
+
+print(running)
+
