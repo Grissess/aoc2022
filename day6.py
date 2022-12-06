d3f2011b74c18241fdfb023d80eeeb396d3a9162
Grissess <grissess@nexusg.org>
2022-12-06 00:19:42 -0500

    Day 6

---
+++ day6.py
@@ -0,0 +1,10 @@
+def som(s, sz=4):
+    for i in range(len(s) - sz):
+        sl = s[i:i+sz]
+        if len(set(sl)) == sz:
+            return i+sz
+    return None
+
+while True:
+    t = input('Message: ')
+    print(som(t))
